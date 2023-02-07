24903421a4b0d8ba34009a7983f12365b1fa0535
cmf <2683095271@qq.com>
2023-02-07 12:08:20 +0800

    First commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Jame
+ * @create 2023-02-07 12:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
